7ade40e923f062097b415295a76b1c3451fb0582
lyutvs <lyutvs@gmail.com>
2022-08-09 22:23:02 +0900

    ⚙️ :: 프로젝트 생성

---
+++ src/test/java/com/moiza/moizaspringbootserver/MoizaSpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moiza.moizaspringbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoizaSpringBootServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
