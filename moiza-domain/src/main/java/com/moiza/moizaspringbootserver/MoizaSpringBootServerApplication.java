4766b06eda552609bbb0cfcf0337190d92ab51e0
lyutvs <lyutvs@gmail.com>
2022-08-16 11:56:07 +0900

    ⚙️ :: 헥사고날 아키텍처

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/MoizaSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.moiza.moizaspringbootserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoizaSpringBootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoizaSpringBootServerApplication.class, args);
+    }
+
+}
