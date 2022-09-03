8b85d69909223670a4f68dcc72d3ec05958af225
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-04 01:38:17 +0900

    ♻️ :: id 필드 추가적인 세팅

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/entity/BaseUUIDEntity.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/entity/BaseUUIDEntity.java
@@ -1,8 +1,10 @@
-package com.moiza.moizaspringbootserver.global;
+package com.moiza.moizaspringbootserver.global.entity;
 
 import com.fasterxml.uuid.Generators;
 import lombok.Getter;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
@@ -11,6 +13,8 @@ import java.util.UUID;
 @MappedSuperclass
 public abstract class BaseUUIDEntity {
 
+    @Id
+    @Column(columnDefinition = "BINARY(16)")
     @NotNull
     private final UUID id = Generators.timeBasedGenerator().generate();
 }
