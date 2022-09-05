4df7a7caffae67cee7103604566113433ad0731c
lyutvs <lyutvs@gmail.com>
2022-09-05 15:58:49 +0900

    ♻️ :: @Repository 명시

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/UserRepository.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.moiza.moizaspringbootserver.domain.user.domain.repository;
 
 import com.moiza.moizaspringbootserver.domain.user.domain.UserEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends CrudRepository<UserEntity, UUID> {
 }
