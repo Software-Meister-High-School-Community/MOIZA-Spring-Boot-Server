b81b07fb7f65c9f38982ca1bc466844b2622e7e1
lyutvs <lyutvs@gmail.com>
2022-09-05 12:16:41 +0900

    📑 :: User

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/User.java
@@ -0,0 +1,28 @@
+package com.moiza.moizaspringbootserver.user;
+
+import com.moiza.moizaspringbootserver.annotation.Aggregate;
+import com.moiza.moizaspringbootserver.user.enums.School;
+import com.moiza.moizaspringbootserver.user.enums.Sex;
+import com.moiza.moizaspringbootserver.user.enums.UserType;
+import lombok.Builder;
+import lombok.Getter;
+
+@Aggregate
+@Getter
+@Builder
+public class User {
+
+    private final String email;
+    private final String accountId;
+    private final String password;
+    private final String name;
+    private final String profileImageUrl;
+    private final String profileBackgroundColor;
+    private final Sex sex;
+    private final Long birthDay;
+    private final String introduce;
+    private final UserType userType;
+    private final School school;
+
+
+}
