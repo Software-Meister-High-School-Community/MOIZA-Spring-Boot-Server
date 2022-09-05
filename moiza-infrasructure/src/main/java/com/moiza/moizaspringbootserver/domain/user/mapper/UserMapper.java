aaa926a1ebf53e398cc23729a5dd02a47a5ccb6d
lyutvs <lyutvs@gmail.com>
2022-09-06 08:44:45 +0900

    ⚡️ :: entity to domain

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/mapper/UserMapper.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/mapper/UserMapper.java
@@ -7,4 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserEntity userDomainToEntity(User user);
+    User userEntityToDomain(UserEntity userEntity);
 }
