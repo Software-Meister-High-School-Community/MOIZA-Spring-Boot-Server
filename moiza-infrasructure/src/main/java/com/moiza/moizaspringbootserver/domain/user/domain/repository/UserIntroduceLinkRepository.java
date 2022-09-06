f80d82d745ba6f22a31452a5712cc657fa2fb344
이준서 <ljchan71@gmail.com>
2022-09-06 11:36:58 +0900

    ⚡️ :: @Repository annotation

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/UserIntroduceLinkRepository.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/UserIntroduceLinkRepository.java
@@ -3,7 +3,9 @@ package com.moiza.moizaspringbootserver.domain.user.domain.repository;
 import com.moiza.moizaspringbootserver.domain.user.domain.UserIntroduceLinkEntity;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserIntroduceLinkRepository extends CrudRepository<UserIntroduceLinkEntity, UUID> {
 
 }
