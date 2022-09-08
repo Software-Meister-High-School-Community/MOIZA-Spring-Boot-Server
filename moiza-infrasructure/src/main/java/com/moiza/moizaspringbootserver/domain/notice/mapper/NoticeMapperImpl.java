976235b3f9a0ea48f407fc1f3301788330946f1f
lyutvs <lyutvs@gmail.com>
2022-09-08 22:38:35 +0900

    📑 :: NoticeMapperImpl

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/notice/mapper/NoticeMapperImpl.java
@@ -0,0 +1,33 @@
+package com.moiza.moizaspringbootserver.domain.notice.mapper;
+
+import com.moiza.moizaspringbootserver.domain.notice.domain.NoticeEntity;
+import com.moiza.moizaspringbootserver.notice.Notice;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NoticeMapperImpl implements NoticeMapper{
+
+    @Override
+    public NoticeEntity noticeDomainToEntity(Notice notice) {
+        return NoticeEntity.builder()
+                .id(notice.getId())
+                .title(notice.getTitle())
+                .content(notice.getContent())
+                .createdAt(notice.getCreatedAt())
+                .updatedAt(notice.getUpdatedAt())
+                .isPinned(notice.isPinned())
+                .build();
+    }
+
+    @Override
+    public Notice noticeEntityToDomain(NoticeEntity noticeEntity) {
+        return Notice.builder()
+                .id(noticeEntity.getId())
+                .title(noticeEntity.getTitle())
+                .content(noticeEntity.getContent())
+                .createdAt(noticeEntity.getCreatedAt())
+                .updatedAt(noticeEntity.getUpdatedAt())
+                .isPinned(noticeEntity.isPinned())
+                .build();
+    }
+}
