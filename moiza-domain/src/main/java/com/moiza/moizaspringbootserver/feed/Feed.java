fbd87efa72aceb949eab4201c2434f49dfeca4b9
VINTO1819 <develretr0@gmail.com>
2022-09-09 17:53:46 +0900

    📑 :: Feed 도메인 및 엔티티

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/Feed.java
@@ -0,0 +1,19 @@
+package com.moiza.moizaspringbootserver.feed;
+
+import com.moiza.moizaspringbootserver.annotation.Aggregate;
+import com.moiza.moizaspringbootserver.feed.enums.FeedType;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+@Builder
+@Aggregate
+public class Feed {
+
+    private final UUID id;
+    private final FeedType feedType;
+    private final UUID userId;
+
+}
---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/domain/FeedEntity.java
@@ -0,0 +1,29 @@
+package com.moiza.moizaspringbootserver.domain.feed.domain;
+
+import com.moiza.moizaspringbootserver.domain.user.domain.UserEntity;
+import com.moiza.moizaspringbootserver.feed.enums.FeedType;
+import com.moiza.moizaspringbootserver.global.entity.BaseUUIDEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_feed")
+public class FeedEntity extends BaseUUIDEntity {
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private FeedType feedType;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private UserEntity user;
+
+}
