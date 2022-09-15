cdb38c760d4212b1ab5a7669e69d51edff33ffbe
lyutvs <lyutvs@gmail.com>
2022-09-15 15:36:18 +0900

    📑 :: SearchHistoryEntity

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/SearchHistoryEntity.java
@@ -0,0 +1,34 @@
+package com.moiza.moizaspringbootserver.domain.user.domain;
+
+import com.moiza.moizaspringbootserver.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+@Entity
+@Table(name = "tbl_search_history")
+public class SearchHistoryEntity extends BaseTimeEntity {
+
+    @NotNull
+    @Length(max = 500)
+    private String keyword;
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity userEntity;
+
+}
