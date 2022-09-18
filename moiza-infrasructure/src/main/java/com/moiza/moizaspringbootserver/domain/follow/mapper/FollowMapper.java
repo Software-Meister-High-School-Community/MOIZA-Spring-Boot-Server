ecec2dbb1b2c42899156bae5ef287ce4a6d4a208
lyutvs <lyutvs@gmail.com>
2022-09-19 00:20:37 +0900

    ⚰️ :: 나.락.

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/mapper/FollowMapper.java
+++
@@ -1,9 +0,0 @@
-package com.moiza.moizaspringbootserver.domain.follow.mapper;
-
-import com.moiza.moizaspringbootserver.domain.follow.domain.FollowEntity;
-import com.moiza.moizaspringbootserver.follow.Follow;
-
-public interface FollowMapper {
-    FollowEntity followDomainToEntity(Follow follow);
-    Follow followEntityToDomain(FollowEntity followEntity);
-}
