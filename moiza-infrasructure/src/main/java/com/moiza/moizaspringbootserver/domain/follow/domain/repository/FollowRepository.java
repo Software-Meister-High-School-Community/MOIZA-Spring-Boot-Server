25e32c62e560224a85dc34b3b8dc216ab546a241
lyutvs <lyutvs@gmail.com>
2022-09-19 00:17:25 +0900

    📑 :: FollowReposiory

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/domain/repository/FollowRepository.java
@@ -0,0 +1,15 @@
+package com.moiza.moizaspringbootserver.domain.follow.domain.repository;
+
+import com.moiza.moizaspringbootserver.domain.follow.domain.FollowEntity;
+import com.moiza.moizaspringbootserver.domain.follow.domain.FollowId;
+import com.moiza.moizaspringbootserver.domain.user.domain.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FollowRepository extends CrudRepository<FollowEntity, FollowId> {
+
+    Optional<FollowEntity> findByFollowIdAndTargetUser(UserEntity user, UserEntity targetUser);
+}
