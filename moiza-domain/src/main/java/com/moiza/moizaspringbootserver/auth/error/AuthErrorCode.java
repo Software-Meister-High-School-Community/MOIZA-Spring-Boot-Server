bf481a927e23a17a636eb3c7eb2e846654e84cd6
이준서 <ljchan71@gmail.com>
2022-09-24 20:22:30 +0900

    📑 :: AuthErrorCode

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/error/AuthErrorCode.java
@@ -0,0 +1,18 @@
+package com.moiza.moizaspringbootserver.auth.error;
+
+import com.moiza.moizaspringbootserver.error.ErrorProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum AuthErrorCode implements ErrorProperty {
+
+	EMAIL_CODE_NOT_FOUND(404, "Email Code Not Found"),
+
+	EMAIL_CODE_NOT_VERIFIED(401, "Email Code Not Verified");
+
+	private final int status;
+	private final String message;
+}
