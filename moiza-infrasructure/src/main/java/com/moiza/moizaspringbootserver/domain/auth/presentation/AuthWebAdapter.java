b6b96ab593e9dced1379def6525f8141eae61d1a
VINTO1819 <develretr0@gmail.com>
2022-09-24 11:23:03 +0900

    📑 :: 어댑터 작성

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/auth/presentation/AuthWebAdapter.java
@@ -0,0 +1,25 @@
+package com.moiza.moizaspringbootserver.domain.auth.presentation;
+
+import com.moiza.moizaspringbootserver.auth.api.IdValidationApi;
+import com.moiza.moizaspringbootserver.auth.api.dto.request.DomainIdValidationRequest;
+import com.moiza.moizaspringbootserver.domain.auth.presentation.dto.request.WebIdValidationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+@RestController
+public class AuthWebAdapter {
+    private final IdValidationApi idValidationApi;
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/", method = RequestMethod.HEAD)
+    public void validId(@RequestBody @Valid WebIdValidationRequest request) {
+        idValidationApi.validId(DomainIdValidationRequest.builder()
+                .accountId(request.getAccountId())
+                .build());
+    }
+}
