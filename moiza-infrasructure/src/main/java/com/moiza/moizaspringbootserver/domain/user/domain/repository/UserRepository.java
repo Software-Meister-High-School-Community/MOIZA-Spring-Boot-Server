d8c61e8e03f5ac1b18abff636aab2d76ac6f816c
이준서 <ljchan71@gmail.com>
2022-09-24 20:23:15 +0900

    ⚡️ :: UserRepository

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/UserRepository.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.moiza.moizaspringbootserver.domain.user.domain.repository;
 
 import com.moiza.moizaspringbootserver.domain.user.domain.UserEntity;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+	Optional<UserEntity> findByAccountId(String accountId);
+	Optional<UserEntity> findByEmail(String email);
 }
