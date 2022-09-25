3d5199ee296aa098709df44963eb1313ba065291
VINTO1819 <develretr0@gmail.com>
2022-09-25 11:09:22 +0900

    📑 :: Security configuration

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/exception/InvalidTokenException.java
@@ -0,0 +1,12 @@
+package com.moiza.moizaspringbootserver.global.exception;
+
+import com.moiza.moizaspringbootserver.error.MoizaException;
+import com.moiza.moizaspringbootserver.global.error.GlobalErrorCode;
+
+public class InvalidTokenException extends MoizaException {
+    private InvalidTokenException() {
+        super(GlobalErrorCode.INVALID_TOKEN);
+    }
+
+    public static InvalidTokenException EXCEPTION = new InvalidTokenException();
+}
---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.moiza.moizaspringbootserver.global.security;
+
+import com.moiza.moizaspringbootserver.global.filter.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .cors()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+
+                // auth
+                .antMatchers(HttpMethod.HEAD, "").permitAll()
+
+                // Deny other requests
+                .anyRequest().denyAll()
+
+                // Add security filter
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
