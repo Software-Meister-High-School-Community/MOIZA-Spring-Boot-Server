32750f0cce15042331392b1717bddc81a41cbfe8
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-25 23:42:11 +0900

    📑 :: UseCase & Adapter 어노테이션

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/annotation/Adapter.java
@@ -0,0 +1,13 @@
+package com.moiza.moizaspringbootserver.domain.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Component
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Adapter { }
