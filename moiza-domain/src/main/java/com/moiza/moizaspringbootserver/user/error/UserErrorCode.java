7a8986d47fa71f1ef0270067412b544a2a9348ae
VINTO1819 <develretr0@gmail.com>
2022-09-25 09:33:15 +0900

    ♻️ :: 오류메시지 영어로 변경

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/error/UserErrorCode.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/error/UserErrorCode.java
@@ -9,8 +9,8 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum UserErrorCode implements ErrorProperty {
 
-	USER_NOT_FOUND(404, "유저를 찾을 수 없음"),
-	USER_ALREADY_EXISTS(409, "이미 해당 ID를 사용하는 유저가 존재합니다.");
+	USER_NOT_FOUND(404, "No such user."),
+	USER_ALREADY_EXISTS(409, "User already exists.");
 
 	private final int status;
 	private final String message;
