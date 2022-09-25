e5a2bd0f96ada26ce4a1dbf7c839b363f9e395a7
VINTO1819 <develretr0@gmail.com>
2022-09-26 08:59:42 +0900

    ♻️ :: 코드리뷰 반영

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/api/dto/response/IdRecoveryResponse.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/api/dto/response/IdRecoveryResponse.java
@@ -3,8 +3,9 @@ package com.moiza.moizaspringbootserver.auth.api.dto.response;
 import lombok.Builder;
 import lombok.Getter;
 
-@Getter @Builder
+@Getter
+@Builder
 public class IdRecoveryResponse {
-    private String accountId;
-    private String name;
+    private final String accountId;
+    private final String name;
 }
