9f727a3ad08847957ec358bf8fc89b9cd7ce4d57
lyutvs <lyutvs@gmail.com>
2022-09-26 10:43:22 +0900

    📑 :: EmailCode

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/EmailCode.java
@@ -0,0 +1,18 @@
+package com.moiza.moizaspringbootserver.auth;
+
+import com.moiza.moizaspringbootserver.annotation.Aggregate;
+import com.moiza.moizaspringbootserver.auth.type.Type;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Aggregate
+public class EmailCode {
+
+    private final String email;
+    private final String authCode;
+    private final Type type;
+    private final boolean isVerify;
+    private final Long timeToLIve;
+}
