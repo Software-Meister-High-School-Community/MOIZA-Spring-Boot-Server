9acb870c293761ad87b6c21a798735aa526d0344
lyutvs <lyutvs@gmail.com>
2022-09-26 17:09:22 +0900

    ⚡️:: AuthWebAdpater

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/auth/presentation/AuthWebAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/auth/presentation/AuthWebAdapter.java
@@ -1,7 +1,9 @@
 package com.moiza.moizaspringbootserver.domain.auth.presentation;
 
 import com.moiza.moizaspringbootserver.auth.api.IdValidationApi;
+import com.moiza.moizaspringbootserver.auth.api.TokenRefreshApi;
 import com.moiza.moizaspringbootserver.auth.api.dto.request.DomainIdValidationRequest;
+import com.moiza.moizaspringbootserver.auth.api.dto.response.TokenRefreshResponse;
 import com.moiza.moizaspringbootserver.domain.auth.presentation.dto.request.WebIdValidationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +16,16 @@ import javax.validation.Valid;
 @RestController
 public class AuthWebAdapter {
     private final IdValidationApi idValidationApi;
+    private final TokenRefreshApi tokenRefreshApi;
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/id-validations", method = RequestMethod.HEAD)
     public void validId(@RequestBody @Valid WebIdValidationRequest request) {
         idValidationApi.execute(new DomainIdValidationRequest(request.getAccountId()));
+    }
+
+    @PutMapping("/tokens")
+    public TokenRefreshResponse tokenRefresh(@RequestHeader("REFRESH-TOKEN") String token) {
+        return tokenRefreshApi.execute(token);
     }
 }
