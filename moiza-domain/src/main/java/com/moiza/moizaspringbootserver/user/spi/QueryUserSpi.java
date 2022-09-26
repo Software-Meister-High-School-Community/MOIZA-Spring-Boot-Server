10002479e092f69af32e2b3166eb5715173b03f4
lyutvs <lyutvs@gmail.com>
2022-09-26 22:55:22 +0900

    ♻️ :: conflict

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/spi/QueryUserSpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/spi/QueryUserSpi.java
@@ -2,10 +2,12 @@ package com.moiza.moizaspringbootserver.user.spi;
 
 import com.moiza.moizaspringbootserver.user.User;
 
+import java.util.UUID;
+
 public interface QueryUserSpi {
 	boolean existsUserByAccountId(String accountId);
 	boolean existsUserByEmail(String email);
 	User queryUserByAccountId(String accountId);
 	User getUserByEmail(String email);
-  User queryUserById(UUID userId);
+  	User queryUserById(UUID userId);
 }
