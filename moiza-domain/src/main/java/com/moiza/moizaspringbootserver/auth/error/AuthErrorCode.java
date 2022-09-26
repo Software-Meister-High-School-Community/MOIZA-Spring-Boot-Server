ed34845c68c7dc7890cfc3e6b6a326a2e9716a42
lyutvs <lyutvs@gmail.com>
2022-09-26 17:07:05 +0900

    ⚡️ :: AuthErrorCode

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/error/AuthErrorCode.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/error/AuthErrorCode.java
@@ -11,7 +11,9 @@ public enum AuthErrorCode implements ErrorProperty {
 
 	EMAIL_CODE_NOT_FOUND(404, "Email Code Not Found"),
 
-	EMAIL_CODE_NOT_VERIFIED(401, "Email Code Not Verified");
+	EMAIL_CODE_NOT_VERIFIED(401, "Email Code Not Verified"),
+
+	REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found");
 
 	private final int status;
 	private final String message;
