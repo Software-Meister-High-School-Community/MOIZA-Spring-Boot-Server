7e9ef956e2835c15aa688c59f8497615e2362117
이준서 <ljchan71@gmail.com>
2022-09-26 11:38:38 +0900

    ⚡️ :: UserPersistenceAdapter

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/persistence/UserPersistenceAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/persistence/UserPersistenceAdapter.java
@@ -4,6 +4,7 @@ import com.moiza.moizaspringbootserver.domain.annotation.Adapter;
 import com.moiza.moizaspringbootserver.domain.user.domain.repository.UserRepository;
 import com.moiza.moizaspringbootserver.domain.user.mapper.UserMapper;
 import com.moiza.moizaspringbootserver.user.User;
+import com.moiza.moizaspringbootserver.user.exception.UserNotFoundException;
 import com.moiza.moizaspringbootserver.user.spi.UserSpi;
 import lombok.RequiredArgsConstructor;
 
@@ -22,6 +23,14 @@ public class UserPersistenceAdapter implements UserSpi {
 	@Override
 	public boolean existsUserByEmail(String email) {
 		return userRepository.findByEmail(email).isPresent();
+	}
+
+	@Override
+	public User queryUserByAccountId(String accountId) {
+		return userMapper.userEntityToDomain(
+			userRepository.findByAccountId(accountId)
+				.orElseThrow(() -> UserNotFoundException.EXCEPTION)
+		);
 	}
 
 	@Override
