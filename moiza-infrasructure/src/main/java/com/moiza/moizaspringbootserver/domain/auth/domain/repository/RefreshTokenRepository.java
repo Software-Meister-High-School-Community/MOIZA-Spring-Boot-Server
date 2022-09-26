2100c7e7a0fa258aa36165b66ece9926059e4637
lyutvs <lyutvs@gmail.com>
2022-09-26 17:08:54 +0900

    ⚡️ :: RefreshTokenRepository

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/auth/domain/repository/RefreshTokenRepository.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -4,6 +4,9 @@ import com.moiza.moizaspringbootserver.domain.auth.domain.RefreshTokenEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, String> {
+    Optional<RefreshTokenEntity> findByRefreshToken(String token);
 }
