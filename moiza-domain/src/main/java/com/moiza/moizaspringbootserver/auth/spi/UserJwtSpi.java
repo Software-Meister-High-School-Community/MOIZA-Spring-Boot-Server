09dd72900485824290ab5fbbccfdaf19ef8b96d7
이준서 <ljchan71@gmail.com>
2022-09-26 19:06:10 +0900

    ⚡️ :: ㅋㄷㅅㅁ

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/spi/UserJwtSpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/spi/UserJwtSpi.java
@@ -1,7 +1,6 @@
 package com.moiza.moizaspringbootserver.auth.spi;
 
 import com.moiza.moizaspringbootserver.auth.spi.dto.TokenResponse;
-import java.util.UUID;
 
 public interface UserJwtSpi {
 	TokenResponse getToken(String email);
