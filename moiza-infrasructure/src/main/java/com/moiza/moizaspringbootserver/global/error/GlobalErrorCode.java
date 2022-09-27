99146cc3a078ad4aa34d52b3265bdd35a3b57636
lyutvs <lyutvs@gmail.com>
2022-09-27 19:26:20 +0900

    ⚡️ :: GlobalErrorCode

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/error/GlobalErrorCode.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/error/GlobalErrorCode.java
@@ -7,8 +7,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum GlobalErrorCode implements ErrorProperty {
-    INVALID_TOKEN(401, "Invalid token");
+    INVALID_TOKEN(401, "Invalid token"),
 
-    int status;
-    String message;
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int status;
+    private final String message;
 }
