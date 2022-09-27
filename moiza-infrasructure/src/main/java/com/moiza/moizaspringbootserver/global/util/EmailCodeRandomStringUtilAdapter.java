6434d3e5438e3485f713983340b7ace11ecd7023
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-27 11:18:55 +0900

    ⚡️ :: 랜덤 문자열 반환 직접 만듬

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/util/EmailCodeRandomStringUtilAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/global/util/EmailCodeRandomStringUtilAdapter.java
@@ -2,13 +2,33 @@ package com.moiza.moizaspringbootserver.global.util;
 
 import com.moiza.moizaspringbootserver.auth.spi.EmailCodeRandomStringSpi;
 import com.moiza.moizaspringbootserver.domain.annotation.Adapter;
-import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Adapter
 public class EmailCodeRandomStringUtilAdapter implements EmailCodeRandomStringSpi {
 
     @Override
     public String getEmailAuthCode() {
-        return RandomStringUtils.randomNumeric(6);
+        List<Character> codeList = new ArrayList<>();
+        List<String> authCodeList = new ArrayList<>();
+
+        codeList.add('1');
+        codeList.add('2');
+        codeList.add('3');
+        codeList.add('4');
+        codeList.add('5');
+        codeList.add('6');
+        codeList.add('7');
+        codeList.add('8');
+        codeList.add('9');
+        codeList.add('0');
+
+        for (int i = 0; i < 6; i++) {
+            authCodeList.add(i, codeList.get((int) (Math.random() * (9 + 1) - 0)).toString());
+        }
+
+        return authCodeList.toString().replaceAll("[^0-9]","");
     }
 }
