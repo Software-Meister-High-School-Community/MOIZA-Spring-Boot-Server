a608391b978fc011653ae594f517f8a23ec4180a
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-28 22:36:05 +0900

    ⚡️ :: 졸업생 인증 api

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/presentation/UserWebAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/presentation/UserWebAdapter.java
@@ -1,10 +1,16 @@
 package com.moiza.moizaspringbootserver.domain.user.presentation;
 
 
+import com.moiza.moizaspringbootserver.domain.user.presentation.dto.request.WebGraduateVerificationRequest;
 import com.moiza.moizaspringbootserver.domain.user.presentation.dto.request.WebUserSignUpRequest;
+import com.moiza.moizaspringbootserver.user.api.GraduateVerificationApi;
 import com.moiza.moizaspringbootserver.user.api.UserSignUpApi;
+import com.moiza.moizaspringbootserver.user.api.dto.request.DomainGraduateVerificationRequest;
 import com.moiza.moizaspringbootserver.user.api.dto.request.DomainUserSignUpRequest;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserWebAdapter {
 
 	private final UserSignUpApi userSignUpApi;
+	private final GraduateVerificationApi graduateVerificationApi;
 
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
@@ -35,5 +42,14 @@ public class UserWebAdapter {
 				.school(request.getSchool())
 				.build()
 		);
+	}
+
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping("/graduate-verifications")
+	public void graduateVerification(@RequestBody @Valid WebGraduateVerificationRequest request) {
+		graduateVerificationApi.execute(
+				DomainGraduateVerificationRequest.builder()
+						.verifyingFileUrl(request.getVerifyingFileUrl())
+						.build());
 	}
 }
