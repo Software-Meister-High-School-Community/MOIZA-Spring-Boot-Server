a092823b51cbe569992e9df3c7853ff419ca5a1d
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-28 18:58:52 +0900

    📑 :: 유저 검색 기록 api & usecase

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/api/UserSearchHistoryApi.java
@@ -0,0 +1,7 @@
+package com.moiza.moizaspringbootserver.user.api;
+
+import com.moiza.moizaspringbootserver.user.api.dto.response.UserSearchHistoryResponse;
+
+public interface UserSearchHistoryApi {
+    UserSearchHistoryResponse execute();
+}
---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/usecase/UserSearchHistoryUseCase.java
@@ -0,0 +1,36 @@
+package com.moiza.moizaspringbootserver.user.usecase;
+
+import com.moiza.moizaspringbootserver.annotation.UseCase;
+import com.moiza.moizaspringbootserver.auth.spi.AuthSecuritySpi;
+import com.moiza.moizaspringbootserver.user.api.UserSearchHistoryApi;
+import com.moiza.moizaspringbootserver.user.api.dto.response.UserSearchHistoryResponse;
+import com.moiza.moizaspringbootserver.user.domain.SearchHistory;
+import com.moiza.moizaspringbootserver.user.spi.QuerySearchHistorySpi;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@UseCase
+public class UserSearchHistoryUseCase implements UserSearchHistoryApi {
+
+    private final AuthSecuritySpi authSecuritySpi;
+    private final QuerySearchHistorySpi querySearchHistorySpi;
+
+    @Override
+    public UserSearchHistoryResponse execute() {
+        UUID userId = authSecuritySpi.getCurrentUserId();
+        List<SearchHistory> searchHistories = querySearchHistorySpi.querySearchHistoryAllByUserId(userId);
+        List<UserSearchHistoryResponse.Keywords> keywords = searchHistories
+                .stream()
+                .map(it -> UserSearchHistoryResponse.Keywords.builder()
+                        .historyId(it.getId())
+                        .keyword(it.getKeyword())
+                        .build()
+                ).collect(Collectors.toList());
+
+        return new UserSearchHistoryResponse(keywords);
+    }
+}
