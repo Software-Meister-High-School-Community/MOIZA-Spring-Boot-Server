13acb42228b62db8761ff67697dcc11a2195f67f
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-28 18:51:32 +0900

    ⚡️ :: jpa 메소드 findAllByUserEntityId

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/SearchHistoryRepository.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/domain/repository/SearchHistoryRepository.java
@@ -4,8 +4,10 @@ import com.moiza.moizaspringbootserver.domain.user.domain.SearchHistoryEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface SearchHistoryRepository extends CrudRepository<SearchHistoryEntity, UUID> {
+    List<SearchHistoryEntity> findAllByUserEntityId(UUID id);
 }
