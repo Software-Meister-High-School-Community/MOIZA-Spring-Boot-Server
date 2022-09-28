a2ce49a78c88041ff0f00422a15af40bf076a1aa
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-28 11:49:52 +0900

    ⚡️ :: search all users api

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/presentation/UserWebAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/user/presentation/UserWebAdapter.java
@@ -2,16 +2,15 @@ package com.moiza.moizaspringbootserver.domain.user.presentation;
 
 
 import com.moiza.moizaspringbootserver.domain.user.presentation.dto.request.WebUserSignUpRequest;
+import com.moiza.moizaspringbootserver.user.api.SearchAllUsersApi;
 import com.moiza.moizaspringbootserver.user.api.UserSignUpApi;
 import com.moiza.moizaspringbootserver.user.api.dto.request.DomainUserSignUpRequest;
 import javax.validation.Valid;
+
+import com.moiza.moizaspringbootserver.user.api.dto.response.SearchAllUsersResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -19,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserWebAdapter {
 
 	private final UserSignUpApi userSignUpApi;
+	private final SearchAllUsersApi searchAllUserApi;
 
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
@@ -35,5 +35,13 @@ public class UserWebAdapter {
 				.school(request.getSchool())
 				.build()
 		);
+	}
+
+	@GetMapping("/searching")
+	public SearchAllUsersResponse searchAllUsers(
+			@RequestParam String name,
+			@RequestParam Integer page
+	) {
+		return searchAllUserApi.execute(name, page);
 	}
 }
