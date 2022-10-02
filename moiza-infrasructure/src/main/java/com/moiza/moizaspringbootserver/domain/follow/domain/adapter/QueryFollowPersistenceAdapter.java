c16e423d163ef4861fa134125d4895cc71d11e7c
lyutvs <lyutvs@gmail.com>
2022-10-03 02:08:26 +0900

    📑 :: QueryFollowPersistenceAdapter

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/domain/adapter/QueryFollowPersistenceAdapter.java
@@ -0,0 +1,26 @@
+package com.moiza.moizaspringbootserver.domain.follow.domain.adapter;
+
+import com.moiza.moizaspringbootserver.domain.annotation.Adapter;
+import com.moiza.moizaspringbootserver.domain.follow.domain.repository.FollowRepository;
+import com.moiza.moizaspringbootserver.domain.user.domain.UserEntity;
+import com.moiza.moizaspringbootserver.domain.user.domain.repository.UserRepository;
+import com.moiza.moizaspringbootserver.follow.spi.QueryFollowSpi;
+import com.moiza.moizaspringbootserver.user.domain.User;
+import com.moiza.moizaspringbootserver.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Adapter
+public class QueryFollowPersistenceAdapter implements QueryFollowSpi {
+
+    private final FollowRepository followRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public Long getFollowCount(User user) {
+        UserEntity userId = userRepository.findById(user.getId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        return followRepository.countAllByUserEntity(userId);
+    }
+}
