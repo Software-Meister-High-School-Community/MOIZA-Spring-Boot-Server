5ca50cbf16617358298f56f8fb6ffd125e558d5b
이준서 <ljchan71@gmail.com>
2022-10-03 01:16:40 +0900

    ⚡️ :: FollowRepository

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/domain/repository/FollowRepository.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/domain/repository/FollowRepository.java
@@ -3,6 +3,8 @@ package com.moiza.moizaspringbootserver.domain.follow.domain.repository;
 import com.moiza.moizaspringbootserver.domain.follow.domain.FollowEntity;
 import com.moiza.moizaspringbootserver.domain.follow.domain.FollowId;
 import com.moiza.moizaspringbootserver.domain.user.domain.UserEntity;
+import java.util.UUID;
+import javax.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,5 @@ import java.util.Optional;
 @Repository
 public interface FollowRepository extends CrudRepository<FollowEntity, FollowId> {
 
-    Optional<FollowEntity> findByFollowIdAndTargetUser(UserEntity user, UserEntity targetUser);
+    Optional<FollowEntity> findByUserEntityIdAndTargetUserId(UUID userId, UUID targetUserId);
 }
