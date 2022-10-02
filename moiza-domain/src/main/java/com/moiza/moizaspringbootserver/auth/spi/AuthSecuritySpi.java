89d53fff872456c5fbba27f59b3d391760fcefad
이준서 <ljchan71@gmail.com>
2022-10-03 01:15:35 +0900

    ⚡️ :: AuthSecuritySpi

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/spi/AuthSecuritySpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/auth/spi/AuthSecuritySpi.java
@@ -1,8 +1,9 @@
 package com.moiza.moizaspringbootserver.auth.spi;
 
+import com.moiza.moizaspringbootserver.follow.sqi.FollowAuthSecuritySpi;
 import java.util.UUID;
 
-public interface AuthSecuritySpi {
+public interface AuthSecuritySpi extends FollowAuthSecuritySpi {
     String encodePassword(String password);
 
     boolean matches(String rawPassword, String encodedPassword);
