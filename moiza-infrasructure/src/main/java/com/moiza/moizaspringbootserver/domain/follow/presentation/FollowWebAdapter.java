e014bb9609d2def45c1127058721dd808538a5c6
이준서 <ljchan71@gmail.com>
2022-10-03 01:16:55 +0900

    ⚡️ :: FolloWebAdapter

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/presentation/FollowWebAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/presentation/FollowWebAdapter.java
@@ -1,8 +1,10 @@
 package com.moiza.moizaspringbootserver.domain.follow.presentation;
 
+import com.moiza.moizaspringbootserver.follow.api.CancelFollowApi;
 import com.moiza.moizaspringbootserver.follow.api.FollowUserApi;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class FollowWebAdapter {
 
 	private final FollowUserApi followUserApi;
+	private final CancelFollowApi cancelFollowApi;
 
 	@PostMapping("/{user-id}")
 	public void followUser(@PathVariable("user-id") UUID userId)  {
 		followUserApi.execute(userId);
+	}
+
+	@DeleteMapping("/{user-id}")
+	public void cancelFollow(@PathVariable("user-id") UUID userId) {
+		cancelFollowApi.execute(userId);
 	}
 }
