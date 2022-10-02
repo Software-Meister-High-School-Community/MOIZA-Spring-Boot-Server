cd0d993139055150a13f947432b871a6213e41df
이준서 <ljchan71@gmail.com>
2022-10-03 01:16:03 +0900

    ⚡️ :: FollowPersistenceAdpater

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/domain/adapter/FollowPersistenceAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/domain/adapter/FollowPersistenceAdapter.java
@@ -1,10 +1,13 @@
 package com.moiza.moizaspringbootserver.domain.follow.domain.adapter;
 
 import com.moiza.moizaspringbootserver.domain.annotation.Adapter;
+import com.moiza.moizaspringbootserver.domain.follow.domain.FollowEntity;
 import com.moiza.moizaspringbootserver.domain.follow.domain.repository.FollowRepository;
 import com.moiza.moizaspringbootserver.domain.follow.mapper.FollowMapper;
 import com.moiza.moizaspringbootserver.follow.Follow;
+import com.moiza.moizaspringbootserver.follow.exception.FollowNotFoundException;
 import com.moiza.moizaspringbootserver.follow.sqi.FollowSpi;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,5 +22,21 @@ public class FollowPersistenceAdapter implements FollowSpi {
 		followRepository.save(
 			followMapper.followDomainToEntity(follow)
 		);
+	}
+
+	@Override
+	public void deleteFollow(Follow follow) {
+		followRepository.delete(
+			followMapper.followDomainToEntity(follow)
+		);
+	}
+
+	@Override
+	public Follow queryFollowByTargetUserId(UUID userId, UUID targetUserId) {
+		FollowEntity followEntity = followRepository
+			.findByUserEntityIdAndTargetUserId(userId, targetUserId)
+			.orElseThrow(() -> FollowNotFoundException.EXCEPTION);
+
+		return null;
 	}
 }
