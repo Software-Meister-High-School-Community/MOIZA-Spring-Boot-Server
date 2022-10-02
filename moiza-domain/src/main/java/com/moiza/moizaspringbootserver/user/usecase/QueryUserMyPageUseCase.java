64853dba52708ec682b098bd93b2276837f9e553
lyutvs <lyutvs@gmail.com>
2022-10-03 02:07:09 +0900

    📑 :: QueryUserMyPageUseCase

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/user/usecase/QueryUserMyPageUseCase.java
@@ -0,0 +1,47 @@
+package com.moiza.moizaspringbootserver.user.usecase;
+
+import com.moiza.moizaspringbootserver.annotation.UseCase;
+import com.moiza.moizaspringbootserver.auth.spi.AuthSecuritySpi;
+import com.moiza.moizaspringbootserver.feed.spi.QueryFeedSqi;
+import com.moiza.moizaspringbootserver.follow.spi.QueryFollowSpi;
+import com.moiza.moizaspringbootserver.user.api.QueryUserMyPageApi;
+import com.moiza.moizaspringbootserver.user.api.dto.response.MyPageResponse;
+import com.moiza.moizaspringbootserver.user.domain.User;
+import com.moiza.moizaspringbootserver.user.spi.QueryUserSpi;
+import com.moiza.moizaspringbootserver.user.spi.UserIntroduceLinkSpi;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@UseCase
+public class QueryUserMyPageUseCase implements QueryUserMyPageApi {
+
+    private final AuthSecuritySpi authSecuritySpi;
+    private final QueryUserSpi queryUserSpi;
+    private final UserIntroduceLinkSpi userIntroduceLinkSpi;
+    private final QueryFeedSqi queryFeedSqi;
+    private final QueryFollowSpi queryFollowSpi;
+
+    @Override
+    public MyPageResponse execute() {
+        User user = queryUserSpi.queryUserById(authSecuritySpi.getCurrentUserId());
+        List<String> userIntroduceLink = userIntroduceLinkSpi.getIntroduceLinkList(user.getId());
+        Long feedCount = queryFeedSqi.getUserFeeCount(user);
+        Long followCount = queryFollowSpi.getFollowCount(user);
+
+        return MyPageResponse.builder()
+                .userId(user.getId())
+                .name(user.getName())
+                .school(user.getSchool())
+                .userScope(user.getUserType())
+                .profileImageUrl(user.getProfileImageUrl())
+                .profileBackgroundColor(user.getProfileBackgroundColor())
+                .linkUrl(userIntroduceLink)
+                .feedCount(feedCount)
+                .followerCount(followCount)
+                .followingCount(followCount)
+                .build();
+    }
+}
+
