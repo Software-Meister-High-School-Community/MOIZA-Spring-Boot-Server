35e2eb810ae1cb7d11274ae79fd4b304d2d56763
이준서 <ljchan71@gmail.com>
2022-10-03 01:15:54 +0900

    ⚡️ :: FollowUserUseCase

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/follow/usecase/FollowUserUseCase.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/follow/usecase/FollowUserUseCase.java
@@ -1,10 +1,10 @@
 package com.moiza.moizaspringbootserver.follow.usecase;
 
 import com.moiza.moizaspringbootserver.annotation.UseCase;
-import com.moiza.moizaspringbootserver.auth.spi.AuthSecuritySpi;
 import com.moiza.moizaspringbootserver.follow.Follow;
 import com.moiza.moizaspringbootserver.follow.api.FollowUserApi;
 import com.moiza.moizaspringbootserver.follow.sqi.CommandFollowSpi;
+import com.moiza.moizaspringbootserver.follow.sqi.FollowAuthSecuritySpi;
 import com.moiza.moizaspringbootserver.follow.sqi.FollowQueryUserSpi;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +14,12 @@ import lombok.RequiredArgsConstructor;
 public class FollowUserUseCase implements FollowUserApi {
 
 	private final FollowQueryUserSpi followQueryUserSpi;
-	private final AuthSecuritySpi authSecuritySpi;
+	private final FollowAuthSecuritySpi followAuthSecuritySpi;
 	private final CommandFollowSpi commandFollowSpi;
 
 	@Override
 	public void execute(UUID userId) {
-		UUID currentUserId = authSecuritySpi.getCurrentUserId();
+		UUID currentUserId = followAuthSecuritySpi.getCurrentUserId();
 		followQueryUserSpi.queryUserById(userId);
 
 		Follow follow = Follow.builder()
