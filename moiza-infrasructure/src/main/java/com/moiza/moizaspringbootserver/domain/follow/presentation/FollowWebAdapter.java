3bc61a82be90f357f27e4246479311126a19f46a
이준서 <ljchan71@gmail.com>
2022-10-03 01:55:34 +0900

    ⚡️ :: FollowWebAdapter

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/presentation/FollowWebAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/follow/presentation/FollowWebAdapter.java
@@ -1,6 +1,7 @@
 package com.moiza.moizaspringbootserver.domain.follow.presentation;
 
 import com.moiza.moizaspringbootserver.follow.api.CancelFollowApi;
+import com.moiza.moizaspringbootserver.follow.api.DeleteFollowApi;
 import com.moiza.moizaspringbootserver.follow.api.FollowUserApi;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -8,27 +9,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/follow")
 @RestController
 public class FollowWebAdapter {
 
 	private final FollowUserApi followUserApi;
 	private final CancelFollowApi cancelFollowApi;
+	private final DeleteFollowApi deleteFollowApi;
 
-	@PostMapping("/{user-id}")
+	@PostMapping("/follow/{user-id}")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void followUser(@PathVariable("user-id") UUID userId)  {
 		followUserApi.execute(userId);
 	}
 
-	@DeleteMapping("/{user-id}")
+	@DeleteMapping("/follow/{user-id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void cancelFollow(@PathVariable("user-id") UUID userId) {
 		cancelFollowApi.execute(userId);
+	}
+
+	@DeleteMapping("/{user-id}/follow")
+	public void deleteFollow(@PathVariable("user-id") UUID userId) {
+		deleteFollowApi.execute(userId);
 	}
 }
