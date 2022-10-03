da38eca0e92c7ddec6ffb47bc635aa04912ffc7e
lyutvs <lyutvs@gmail.com>
2022-10-03 19:02:15 +0900

    ⚡️ :: UserReportMapper

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/report/mapper/user/UserReportMapper.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/report/mapper/user/UserReportMapper.java
@@ -1,9 +1,9 @@
 package com.moiza.moizaspringbootserver.domain.report.mapper.user;
 
 import com.moiza.moizaspringbootserver.domain.report.domain.user.UserReportEntity;
-import com.moiza.moizaspringbootserver.report.user.UserReport;
+import com.moiza.moizaspringbootserver.report.user.domain.UserReport;
 
 public interface UserReportMapper {
-    UserReportEntity feedReportDomainToEntity(UserReport userReport);
-    UserReport feedReportEntityToDomain(UserReportEntity userReportEntity);
+    UserReportEntity userReportDomainToEntity(UserReport userReport);
+    UserReport userReportEntityToDomain(UserReportEntity userReportEntity);
 }
