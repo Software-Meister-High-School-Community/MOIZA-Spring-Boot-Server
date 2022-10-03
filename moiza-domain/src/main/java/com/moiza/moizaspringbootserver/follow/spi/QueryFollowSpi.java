864981acf8760b6aa46d98072a8c36eee3649991
lyutvs <lyutvs@gmail.com>
2022-10-03 23:23:03 +0900

    ⚡️ :: QueryFollowSpi

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/follow/spi/QueryFollowSpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/follow/spi/QueryFollowSpi.java
@@ -2,8 +2,6 @@ package com.moiza.moizaspringbootserver.follow.spi;
 
 import com.moiza.moizaspringbootserver.user.domain.User;
 
-import java.util.UUID;
-
 public interface QueryFollowSpi {
     Long getFollowCount(User user);
 }
