cba06e55c4c76d7c55c0a2c2af6ecfc54e41a07e
lyutvs <lyutvs@gmail.com>
2022-10-16 18:05:46 +0900

    📑 :: WebFeedAdapter

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/presentation/WebFeedAdapter.java
@@ -0,0 +1,22 @@
+package com.moiza.moizaspringbootserver.domain.feed.presentation;
+
+import com.moiza.moizaspringbootserver.feed.api.DeleteFeedApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RequestMapping("/feeds")
+@RestController
+public class WebFeedAdapter {
+
+    private final DeleteFeedApi deleteFeedApi;
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{feed-id}")
+    public void deleteFeed(@PathVariable("feed-id") UUID feedId) {
+        deleteFeedApi.execute(feedId);
+    }
+}
