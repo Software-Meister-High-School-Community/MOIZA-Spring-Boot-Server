e04884ffa77c6c884e8b8698c1e93b775d31907d
lyutvs <lyutvs@gmail.com>
2022-10-16 18:07:08 +0900

    📑 :: FeedLikeId

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/like/domain/FeedLikeId.java
@@ -0,0 +1,20 @@
+package com.moiza.moizaspringbootserver.domain.like.domain;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Embeddable
+@EqualsAndHashCode
+public class FeedLikeId implements Serializable {
+
+    private UUID user;
+
+    private UUID feed;
+
+}
