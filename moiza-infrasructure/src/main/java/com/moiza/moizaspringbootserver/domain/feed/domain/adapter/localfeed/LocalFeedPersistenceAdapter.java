d3c56e3aeb89de91d66005b87bc36a40d69b7886
lyutvs <lyutvs@gmail.com>
2022-10-16 18:08:19 +0900

    📑 :: LocalFeedPersistenceAdapter

---
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/domain/adapter/localfeed/LocalFeedPersistenceAdapter.java
@@ -0,0 +1,23 @@
+package com.moiza.moizaspringbootserver.domain.feed.domain.adapter.localfeed;
+
+import com.moiza.moizaspringbootserver.domain.annotation.Adapter;
+import com.moiza.moizaspringbootserver.domain.feed.domain.repository.LocalFeedRepository;
+import com.moiza.moizaspringbootserver.domain.feed.mapper.FeedMapper;
+import com.moiza.moizaspringbootserver.feed.Feed;
+import com.moiza.moizaspringbootserver.feed.spi.localfeed.LocalFeedSpi;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Adapter
+public class LocalFeedPersistenceAdapter implements LocalFeedSpi {
+
+    private final FeedMapper feedMapper;
+    private final LocalFeedRepository localFeedRepository;
+
+    @Override
+    public void localFeedDelete(Feed feed) {
+        localFeedRepository.deleteByFeed(
+                feedMapper.feedDomainToEntity(feed)
+        );
+    }
+}
