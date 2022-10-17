5a532e82e4a01e3a141a98787a34d3698cc4ec6c
lyutvs <lyutvs@gmail.com>
2022-10-17 12:28:58 +0900

    ♻️ :: codesmell

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/comment/spi/CommandCommentAttachmentFileSpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/comment/spi/CommandCommentAttachmentFileSpi.java
@@ -1,7 +1,6 @@
 package com.moiza.moizaspringbootserver.comment.spi;
 
 import com.moiza.moizaspringbootserver.comment.Comment;
-import com.moiza.moizaspringbootserver.feed.Feed;
 
 public interface CommandCommentAttachmentFileSpi {
     void deleteCommentAttachmentFile(Comment comment);
