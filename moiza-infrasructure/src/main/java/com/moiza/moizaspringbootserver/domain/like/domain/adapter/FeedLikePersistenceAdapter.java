d47a30a7890a8feca95c65edb9766f0e00835af8
Jeong Yoon <dlwjddbs14@naver.com>
2022-10-18 09:18:20 +0900

    ⚡️ :: feed like adapter 들들

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/like/domain/adapter/FeedLikePersistenceAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/like/domain/adapter/FeedLikePersistenceAdapter.java
@@ -2,21 +2,40 @@ package com.moiza.moizaspringbootserver.domain.like.domain.adapter;
 
 import com.moiza.moizaspringbootserver.domain.annotation.Adapter;
 import com.moiza.moizaspringbootserver.domain.feed.mapper.FeedMapper;
+import com.moiza.moizaspringbootserver.domain.like.domain.FeedLikeId;
 import com.moiza.moizaspringbootserver.domain.like.domain.repository.FeedLikeRepository;
+import com.moiza.moizaspringbootserver.domain.like.mapper.FeedLikeMapper;
 import com.moiza.moizaspringbootserver.feed.Feed;
+import com.moiza.moizaspringbootserver.like.FeedLike;
 import com.moiza.moizaspringbootserver.like.spi.FeedLikeSpi;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Adapter
 public class FeedLikePersistenceAdapter implements FeedLikeSpi {
     private final FeedMapper feedMapper;
     private final FeedLikeRepository feedLikeRepository;
+    private final FeedLikeMapper feedLikeMapper;
+
+    @Override
+    public void saveFeedLike(FeedLike feedLike) {
+        feedLikeRepository.save(
+                feedLikeMapper.feedLikeDomainToEntity(feedLike)
+        );
+    }
 
     @Override
     public void deleteFeedLike(Feed feed) {
         feedLikeRepository.deleteByFeed(
                 feedMapper.feedDomainToEntity(feed)
         );
+    }
+
+    @Override
+    public boolean existsByFeedIdAndUserId(UUID feedId, UUID userId) {
+        return feedLikeRepository.findById(new FeedLikeId(userId, feedId, LocalDateTime.now())).isPresent();
     }
 }
