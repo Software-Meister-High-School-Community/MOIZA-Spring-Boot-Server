ee0a1b0f4c9e611243c84be0a466e70bb15799f9
ericlee05 <develretr0@gmail.com>
2022-10-19 18:00:07 +0900

    ⚡️ :: Feed Adapter

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/presentation/WebFeedAdapter.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/presentation/WebFeedAdapter.java
@@ -1,6 +1,7 @@
 package com.moiza.moizaspringbootserver.domain.feed.presentation;
 
 import com.moiza.moizaspringbootserver.feed.api.DeleteFeedApi;
+import com.moiza.moizaspringbootserver.like.api.DiscardFeedLikeApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +14,17 @@ import java.util.UUID;
 public class WebFeedAdapter {
 
     private final DeleteFeedApi deleteFeedApi;
+    private final DiscardFeedLikeApi discardFeedLikeApi;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{feed-id}")
     public void deleteFeed(@PathVariable("feed-id") UUID feedId) {
         deleteFeedApi.execute(feedId);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{feed-id}/like")
+    public void addFeedLike(@PathVariable(name = "feed-id") UUID feedId) {
+        discardFeedLikeApi.execute(feedId);
     }
 }
