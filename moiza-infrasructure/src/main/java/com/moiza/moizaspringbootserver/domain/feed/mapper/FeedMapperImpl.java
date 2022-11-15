2844ccef616bf5f81f929382e705b581d6eca2b1
junseo <ljchan71@gmail.com>
2022-11-15 23:12:15 +0900

    ⚡️:: FeedMapperImpl

--- moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/mapper/FeedMapperImpl.java
+++ moiza-infrasructure/src/main/java/com/moiza/moizaspringbootserver/domain/feed/mapper/FeedMapperImpl.java
@@ -1,7 +1,11 @@
 package com.moiza.moizaspringbootserver.domain.feed.mapper;
 
 import com.moiza.moizaspringbootserver.domain.feed.domain.FeedEntity;
+import com.moiza.moizaspringbootserver.domain.feed.domain.repository.CategoryRepository;
+import com.moiza.moizaspringbootserver.domain.user.domain.repository.UserRepository;
 import com.moiza.moizaspringbootserver.feed.Feed;
+import com.moiza.moizaspringbootserver.feed.exception.CategoryNotFoundException;
+import com.moiza.moizaspringbootserver.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +13,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class FeedMapperImpl implements FeedMapper {
 
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
     @Override
     public FeedEntity feedDomainToEntity(Feed feed) {
         return FeedEntity.builder()
                 .id(feed.getId())
                 .feedType(feed.getFeedType())
+                .user(
+                        userRepository.findById(feed.getUserId())
+                                .orElseThrow(() -> UserNotFoundException.EXCEPTION)
+                )
+                .category(
+                        categoryRepository.findById(feed.getCategoryId())
+                                .orElseThrow(() -> CategoryNotFoundException.EXCEPTION)
+                )
                 .build();
     }
 
@@ -22,6 +37,7 @@ public class FeedMapperImpl implements FeedMapper {
         return Feed.builder()
                 .id(feedEntity.getId())
                 .feedType(feedEntity.getFeedType())
+                .categoryId(feedEntity.getCategory().getId())
                 .build();
     }
 }
