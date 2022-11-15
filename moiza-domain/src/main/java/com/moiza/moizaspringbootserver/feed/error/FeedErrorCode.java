a19c476a27f076b5da1b4c00fd084a1c2998abfe
junseo <ljchan71@gmail.com>
2022-11-15 23:10:52 +0900

    ⚡️:: FeedErrorCode

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/error/FeedErrorCode.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/error/FeedErrorCode.java
@@ -9,7 +9,8 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum FeedErrorCode implements ErrorProperty {
 	BAD_FEED_TYPE(400, "No Such Feed Type"),
-	FEED_NOT_FOUND(404, "feed is not found");
+	FEED_NOT_FOUND(404, "feed is not found"),
+	CATEGORY_NOT_FOUND(404, "Category is not found");
 
 	private final int status;
 	private final String message;
