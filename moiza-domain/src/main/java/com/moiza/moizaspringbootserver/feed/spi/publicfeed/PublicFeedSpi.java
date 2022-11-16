55f0d44c54637d44e1ba78da82649900e591c471
junseo <ljchan71@gmail.com>
2022-11-16 19:08:55 +0900

    ⚡️ :: PublicFeedSpi

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/spi/publicfeed/PublicFeedSpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/spi/publicfeed/PublicFeedSpi.java
@@ -1,4 +1,4 @@
 package com.moiza.moizaspringbootserver.feed.spi.publicfeed;
 
-public interface PublicFeedSpi extends CommandPublicFeedSpi, PublicFeedQuerySpi {
+public interface PublicFeedSpi extends CommandPublicFeedSpi, QueryPublicFeedSpi {
 }
