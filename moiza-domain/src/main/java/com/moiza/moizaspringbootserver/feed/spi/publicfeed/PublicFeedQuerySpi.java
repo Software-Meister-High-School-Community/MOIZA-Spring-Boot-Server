5c0d7f7affa11e4bdcbc9af6bfa53384f72152e8
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-16 12:06:08 +0900

    ⚡️ :: Feed Query Spi

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/spi/publicfeed/PublicFeedQuerySpi.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/spi/publicfeed/PublicFeedQuerySpi.java
@@ -1,11 +1,15 @@
 package com.moiza.moizaspringbootserver.feed.spi.publicfeed;
 
+import com.moiza.moizaspringbootserver.feed.PublicFeed;
 import com.moiza.moizaspringbootserver.feed.enums.FeedType;
 import com.moiza.moizaspringbootserver.feed.spi.dto.response.PublishedFeedPage;
 import com.moiza.moizaspringbootserver.feed.spi.publicfeed.type.QueryOrders;
 
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.UUID;
 
 public interface PublicFeedQuerySpi {
     PublishedFeedPage getAllPublishedFeed(UUID userId, String category, FeedType type, QueryOrders order, int page);
+    List<PublicFeed> getPublicFeedsByCategory(String category);
 }
