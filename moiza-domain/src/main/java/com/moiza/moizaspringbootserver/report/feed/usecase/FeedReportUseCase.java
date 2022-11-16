a1f3044b73d4ef5a491dfb9c1e2f2f6b3621badd
junseo <ljchan71@gmail.com>
2022-11-16 20:03:05 +0900

    📝 :: FeedReportUseCase

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/report/feed/usecase/FeedReportUseCase.java
@@ -0,0 +1,38 @@
+package com.moiza.moizaspringbootserver.report.feed.usecase;
+
+import com.moiza.moizaspringbootserver.annotation.UseCase;
+import com.moiza.moizaspringbootserver.auth.spi.AuthSecuritySpi;
+import com.moiza.moizaspringbootserver.report.Report;
+import com.moiza.moizaspringbootserver.report.feed.FeedReport;
+import com.moiza.moizaspringbootserver.report.feed.api.FeedReportApi;
+import com.moiza.moizaspringbootserver.report.feed.spi.CommandFeedReportSpi;
+import com.moiza.moizaspringbootserver.report.spi.CommandReportSpi;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@UseCase
+public class FeedReportUseCase implements FeedReportApi {
+
+    private final AuthSecuritySpi authSecuritySpi;
+
+    private final CommandReportSpi commandReportSpi;
+
+    private final CommandFeedReportSpi commandFeedReportSpi;
+
+    @Override
+    public void execute(UUID feedId) {
+        UUID id = authSecuritySpi.getCurrentUserId();
+
+        Report report = commandReportSpi.saveReport(Report.builder()
+                .reportedUserId(id)
+                .build()
+        );
+        FeedReport feedReport = FeedReport.builder()
+                .reportId(report.getId())
+                .feedId(feedId)
+                .build();
+        commandFeedReportSpi.saveFeedReport(feedReport);
+    }
+}
