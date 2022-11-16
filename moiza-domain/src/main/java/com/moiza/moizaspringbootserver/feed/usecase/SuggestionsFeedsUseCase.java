eaadf1b089814a1774e337bc56fd519ededf3bfe
Jeong Yoon <dlwjddbs14@naver.com>
2022-11-16 12:29:19 +0900

    ♻️ :: import 제거

--- moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/usecase/SuggestionsFeedsUseCase.java
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/usecase/SuggestionsFeedsUseCase.java
@@ -1,27 +1,18 @@
 package com.moiza.moizaspringbootserver.feed.usecase;
 
 import com.moiza.moizaspringbootserver.annotation.UseCase;
-import com.moiza.moizaspringbootserver.auth.spi.AuthSecuritySpi;
-import com.moiza.moizaspringbootserver.feed.Feed;
 import com.moiza.moizaspringbootserver.feed.PublicFeed;
 import com.moiza.moizaspringbootserver.feed.api.SuggestionsFeedsApi;
 import com.moiza.moizaspringbootserver.feed.api.dto.response.SuggestionsFeedsResponse;
 import com.moiza.moizaspringbootserver.feed.api.dto.response.SuggestionsFeedsResponse.SuggestionFeed;
-import com.moiza.moizaspringbootserver.feed.spi.feed.FeedSpi;
 import com.moiza.moizaspringbootserver.feed.spi.feed.QueryFeedSpi;
 import com.moiza.moizaspringbootserver.feed.spi.publicfeed.PublicFeedQuerySpi;
-import com.moiza.moizaspringbootserver.user.domain.User;
 import com.moiza.moizaspringbootserver.user.spi.QueryUserSpi;
 import lombok.RequiredArgsConstructor;
 
-import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-import java.util.UUID;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @UseCase
