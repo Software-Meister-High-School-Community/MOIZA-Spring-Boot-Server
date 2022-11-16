c58f5293276b0ca9df55effe372e168a34d72c93
junseo <ljchan71@gmail.com>
2022-11-16 19:08:26 +0900

    📑 :: QueryFeedSpi

---
+++ moiza-domain/src/main/java/com/moiza/moizaspringbootserver/feed/spi/feed/QueryFeedSpi.java
@@ -0,0 +1,9 @@
+package com.moiza.moizaspringbootserver.feed.spi.feed;
+
+import com.moiza.moizaspringbootserver.feed.Feed;
+
+import java.util.UUID;
+
+public interface QueryFeedSpi {
+    Feed queryFeedById(UUID feedId);
+}
